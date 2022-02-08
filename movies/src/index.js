e06b5a814f238e91274141f2596579d4dde61fbc
Jack McGrath <jackmcgrath2k@gmail.com>
2022-02-08 10:35:08 +0000

    Movie Details Page - included production countries

--- movies/src/index.js
+++ movies/src/index.js
@@ -70,9 +70,14 @@ const sample = {
   ],
   production_countries: [
     {
-      iso_3166_1: "US",
-      name: "United States of America",
+      iso_3166_1: "FR",
+      name: "France"
     },
+    {
+      iso_3166_2: "LIT",
+      name: "Lithuania"
+     
+    }
   ],
   release_date: "2017-12-13",
   revenue: 1332459537,
